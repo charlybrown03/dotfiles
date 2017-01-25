6bd6af638e1c20d2afdacdf2f4e41e77d97a57e1
Germán Robledo Curiel <germix@germix.net>
2017-01-25 03:40:13 +0100

    Fix some macOs defaults

--- macos/keyboard.sh
+++ macos/keyboard.sh
@@ -11,12 +11,12 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 # Off: 300000
 # Slow: 120
 # Fast: 2
-defaults write NSGlobalDomain KeyRepeat -int 40
+defaults write NSGlobalDomain KeyRepeat -int 10
 
 # Set delay until repeat (in milliseconds)
 # Long: 120
 # Short: 15
-defaults write NSGlobalDomain InitialKeyRepeat -int 40
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # Adjust keyboard brightness in low light
 defaults write com.apple.BezelServices kDim -bool true
--- macos/other.sh
+++ macos/other.sh
@@ -34,7 +34,7 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
 # Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
-/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+#/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
 
 # Display ASCII control characters using caret notation in standard text views
 # Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
