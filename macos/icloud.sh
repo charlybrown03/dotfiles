13ece9089c160c07ad16ccac757b636cd9f430d2
germanrcuriel <germix@germix.net>
2017-11-14 10:17:54 -0800

    Remove macos defaults

--- macos/icloud.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-###############################################################################
-# iCloud                                                                      #
-###############################################################################
-
-# Save to iCloud by default
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool true
--- macos/notifications.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-###############################################################################
-# Notifications                                                               #
-###############################################################################
-
-# Disable Notification Center and remove the menu bar icon
-#launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
