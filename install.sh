e164702d5f5baeff293fc06bec69f95ae8e6270d
Carlos Moreno <carlos.moreno.villodre@gmail.com>
2018-02-14 23:00:09 +0000

    Cleanup useless stuff and set Debian and Arch install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Debian distrubutions
+# sudo apt install autojump git htop node wget atom docker vagrant virtualbox
+
+# Arch distrubutions
+sudo pacman -Sy autojump git htop node wget atom docker vagrant virtualbox
