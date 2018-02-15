1d2cc2465aaf514852f03c5a8de6dbdd1868654d
Carlos Moreno <carlos.moreno.villodre@gmail.com>
2018-02-15 11:31:21 +0100

    Add Linux support to copy file

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Debian distrubutions
-# sudo apt install autojump git htop node wget atom docker vagrant virtualbox
+# sudo apt install autojump git htop node wget atom docker vagrant virtualbox xsel
 
 # Arch distrubutions
-sudo pacman -Sy autojump git htop node wget atom docker vagrant virtualbox
+sudo pacman -Sy autojump git htop node wget atom docker vagrant virtualbox xsel
