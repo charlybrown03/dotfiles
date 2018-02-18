1af9c054ff24ddac650f8410f70c9f5a7643b499
Carlos <carlos.moreno.villodre@gmail.com>
2018-02-18 02:08:05 +0100

    Go back to Debian

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Debian distrubutions
-# sudo apt install autojump git htop node wget atom docker vagrant virtualbox xsel
-
-# Arch distrubutions
-sudo pacman -Sy autojump git htop node wget atom docker vagrant virtualbox xsel
